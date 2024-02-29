5f5f13a663b0971d0bc81e8cf7efda84d3bb3324
oixm <oixm@hotmail.com>
2024-02-29 10:48:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""Simple Flash app."""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """Response text"""
     return 'Hello, world!'
 
 if __name__ == '__main__':
